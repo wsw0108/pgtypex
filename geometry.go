c03e6bf58319a9036b8ecb1143f40ea4f7c192c0
wsw0108 <wsw0108@gmail.com>
2020-10-29 09:31:33 +0800

    Encode/Decode to/from Text, prefer binary format

--- geometry.go
+++ geometry.go
@@ -3,9 +3,11 @@ package pgtypex
 import (
 	"bytes"
 
+	wktparser "github.com/Succo/wktToOrb"
 	"github.com/jackc/pgtype"
 	"github.com/paulmach/orb"
 	"github.com/paulmach/orb/encoding/wkb"
+	"github.com/paulmach/orb/encoding/wkt"
 )
 
 type Geometry struct {
@@ -16,6 +18,17 @@ func NewGeometry(geom orb.Geometry) *Geometry {
 	return &Geometry{geom}
 }
 
+func (Geometry) PreferredParamFormat() int16 {
+	return pgtype.BinaryFormatCode
+}
+
+func (g *Geometry) EncodeText(ci *pgtype.ConnInfo, buf []byte) ([]byte, error) {
+	// FIXME: use buf
+	// w := bytes.NewBuffer(buf)
+	wktString := wkt.MarshalString(g.Geom)
+	return []byte(wktString), nil
+}
+
 func (g *Geometry) EncodeBinary(ci *pgtype.ConnInfo, buf []byte) ([]byte, error) {
 	w := bytes.NewBuffer(buf)
 	enc := wkb.NewEncoder(w)
@@ -23,6 +36,20 @@ func (g *Geometry) EncodeBinary(ci *pgtype.ConnInfo, buf []byte) ([]byte, error)
 		return nil, err
 	}
 	return w.Bytes(), nil
+}
+
+func (Geometry) PreferredResultFormat() int16 {
+	return pgtype.BinaryFormatCode
+}
+
+func (g *Geometry) DecodeText(ci *pgtype.ConnInfo, src []byte) (err error) {
+	wktString := string(src)
+	geom, err := wktparser.Scan(wktString)
+	if err != nil {
+		return
+	}
+	g.Geom = geom
+	return
 }
 
 func (g *Geometry) DecodeBinary(ci *pgtype.ConnInfo, src []byte) (err error) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wsw0108/pgtypex
 go 1.13
 
 require (
+	github.com/Succo/wktToOrb v0.0.0-20190124223054-a153a63ad351
 	github.com/jackc/pgtype v1.3.0
 	github.com/paulmach/orb v0.1.6
 )
